c67a908cdda8b237718dd6de55e0f58450c318c2
Youngju Jaden Kim <pydemia@gmail.com>
2026-04-13 15:56:30 +0900

    Create lazy_nvm.sh

---
+++ lazy_nvm.sh
@@ -0,0 +1,21 @@
+
+# ORIGINAL: Eager-load NVM
+#export NVM_DIR="$HOME/.nvm"
+#[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+#[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+#export NVM_DIR="$HOME/.nvm"
+#[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+#[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+
+# REVISED: Lazy-load NVM
+export NVM_DIR="$HOME/.nvm"
+nvm() {
+  unset -f nvm node npm npx
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+  nvm "$@"
+}
+
+node() { nvm > /dev/null; node "$@" }
+npm() { nvm > /dev/null; npm "$@" }
+npx() { nvm > /dev/null; npx "$@" }
